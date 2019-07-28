d3416c39b7d6fd478e546ea90b4f1664de143b4a
Ana <angy.aa@gmail.com>
2019-07-28 14:47:03 +0200

    Create numbers list component

--- src/view/App.js
+++ src/view/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import '../App.css';
 import { Header } from './common/header/Header';
+import { NumbersList } from './section-one/NumbersList';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <p>App works</p>
+        <NumbersList />
       </main>
     </>
   );
---
+++ src/view/section-one/NumbersList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// import 'NumbersList.css';
+
+class NumbersList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>Numbers list works</div>
+        )
+    }
+}
+
+export { NumbersList }
